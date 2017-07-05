471a01829722ef42c71cc8b4b2d141285493bd9a
Macbook Pro <macbookpro@Mateusz-Pogorzelski-Macbook-Pro.local>
2017-07-05 20:49:33 +0200

    kategorie

--- src/components/EventCategories.js
+++ src/components/EventCategories.js
@@ -4,9 +4,65 @@
 import React from 'react'
 
 export default class EventCategories extends React.Component {
-  render() {
-    return (
-      <h1>EventCategories</h1>
+    render() {
+        return (
+        <h1>EventCategories</h1>
+        < div
+    class
+        = "list-group" >
+            // <a href="#" class="list-group-item disabled"> </a>
+            < a
+        href = "#"
+    class
+        = "list-group-item" > Pop < / a >
+            < a
+        href = "#"
+    class
+        = "list-group-item" > Hip - hop < / a >
+            < a
+        href = "#"
+    class
+        = "list-group-item" > Jazz < / a >
+            < a
+        href = "#"
+    class
+        = "list-group-item" > ALternative < / a >
+            < a
+        href = "#"
+    class
+        = "list-group-item" > Rock < / a >
+            < a
+        href = "#"
+    class
+        = "list-group-item" > Disco
+        polo < / a >
+        < a
+        href = "#"
+    class
+        = "list-group-item" > House < / a >
+            < a
+        href = "#"
+    class
+        = "list-group-item" > Metal < / a >
+            < a
+        href = "#"
+    class
+        = "list-group-item" > Techno < / a >
+            < a
+        href = "#"
+    class
+        = "list-group-item" > Blues < / a >
+            < a
+        href = "#"
+    class
+        = "list-group-item" > Drum & Base < / a >
+            < / div >
+    )
+    }
+}
+
+
+
     )
   }
 }
