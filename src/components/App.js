2ea8d41ec10f4415f3589741aad32d69ac28ee49
wojciktomasz <wojciktomasz@o2.pl>
2017-07-05 16:03:46 +0200

    MainMenu ready

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import MainMenu from './MainMenu'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+      <MainMenu/>
       </div>
     );
   }
