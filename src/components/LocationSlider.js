07a24153a5fc97b865c9aac598a05843252284f2
wojciktomasz <wojciktomasz@o2.pl>
2017-07-06 16:55:22 +0200

    results version 1

--- src/components/LocationSlider.js
+++ src/components/LocationSlider.js
@@ -4,9 +4,7 @@
 import React from 'react'
 import Slider from 'rc-slider'
 import 'rc-slider/assets/index.css'
-import {Grid,
-  Row,
-  Col,} from 'react-bootstrap'
+import {Col} from 'react-bootstrap'
 
 export default class LocationSlider extends React.Component {
   render() {
