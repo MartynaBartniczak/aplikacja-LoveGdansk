0d6d7c55be86379759988f1b6499fe863895d017
wojciktomasz <wojciktomasz@o2.pl>
2017-07-07 16:30:59 +0200

    Calendar slider

--- src/components/LocationSlider.js
+++ src/components/LocationSlider.js
@@ -7,9 +7,11 @@ import 'rc-slider/assets/index.css'
 import {Col} from 'react-bootstrap'
 
 export default class LocationSlider extends React.Component {
+
   render() {
     return (
-      <Col xs={12} sm={6}><h2>Wybierz odległość</h2><Slider/></Col>
+
+      <Col xs={12} sm={6}><h3>Wybierz odległość</h3><Slider/></Col>
     )
   }
 }
--- src/components/TimeSlider.js
+++ src/components/TimeSlider.js
@@ -5,12 +5,21 @@ import React from 'react'
 import Slider from 'rc-slider'
 import 'rc-slider/assets/index.css'
 import {Col} from 'react-bootstrap'
+import moment from 'moment'
 
 export default class TimeSlider extends React.Component {
+  state = {
+    sliderValue: 0
+  }
+
+  handleChange = value => this.setState({
+    sliderValue: value
+  })
+
   render() {
     return (
-      <Col xs={12} sm={6}><h2>Wybierz datę</h2>
-        <Slider/>
+      <Col xs={12} sm={6}><h3>Wybierz datę {moment().add(this.state.sliderValue, 'days').calendar()}</h3>
+        <Slider min={0} max={10} value={this.state.sliderValue} onChange={this.handleChange} boolean={false}/>
       </Col>
     )
   }
