e5a3b382a61abf70a5d280aed845844f334e04ea
wojciktomasz <wojciktomasz@o2.pl>
2017-07-10 16:09:32 +0200

    fetch daat from music.json

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -12,51 +12,43 @@ import balloonImg from './img/baloons.jpg';
 import './SearchResults.css'
 
 export default class SearchResults extends React.Component {
+
+  componentWillMount() {
+    fetch(
+      'http://localhost:3000/data/music.json'
+    ).then(
+      response => response.json()
+    ).then(
+      data => this.setState({
+        events: data
+      })
+    ).catch(
+      error => console.log(error.message)
+    )
+  }
+
   render() {
     return (
       <div className="mainresults">
       <Grid>
         <Row>
-          <Col xs={12} md={6}>
-            <Thumbnail src={balloonImg} alt="242x200">
-              <h3>Szalona impreza z balonami</h3>
-              <p>Ballons party hard</p>
-              <p>
-                <Button bsStyle="primary">Zobacz szczegóły</Button>&nbsp;
-                <Button bsStyle="default">Dodaj do kalendarza</Button>
-              </p>
-            </Thumbnail>
-          </Col>
-          <Col xs={12} md={6}>
-            <Thumbnail src={balloonImg} alt="242x200">
-              <h3>Szalona impreza z balonami 2</h3>
-              <p>Ballons party hard</p>
-              <p>
-                <Button bsStyle="primary">Zobacz szczegóły</Button>&nbsp;
-                <Button bsStyle="default">Dodaj do kalendarza</Button>
-              </p>
-            </Thumbnail>
-          </Col>
-          <Col xs={12} md={6}>
-            <Thumbnail src={balloonImg} alt="242x200">
-              <h3>Szalona impreza z balonami 3</h3>
-              <p>Ballons party hard</p>
-              <p>
-                <Button bsStyle="primary">Zobacz szczegóły</Button>&nbsp;
-                <Button bsStyle="default">Dodaj do kalendarza</Button>
-              </p>
-            </Thumbnail>
-          </Col>
-          <Col xs={12} md={6}>
-            <Thumbnail src={balloonImg} alt="242x200">
-              <h3>Szalona impreza z balonami 4</h3>
-              <p>Ballons party hard</p>
-              <p>
-                <Button bsStyle="primary">Zobacz szczegóły</Button>&nbsp;
-                <Button bsStyle="default">Dodaj do kalendarza</Button>
-              </p>
-            </Thumbnail>
-          </Col>
+              { this.state === null ? <p>Fetching data ....</p> : null}
+              {
+                this.state !== null && this.state.events.slice(1,10).map(
+                  event => (
+                      <Col xs={12} md={6}>
+                        <Thumbnail src={balloonImg} alt="242x200">
+                      {event.category}
+                          <p>Ballons party hard</p>
+                          <p>
+                            <Button bsStyle="primary">Zobacz szczegóły</Button>&nbsp;
+                            <Button bsStyle="default">Dodaj do kalendarza</Button>
+                          </p>
+                        </Thumbnail>
+                      </Col>
+                  )
+                )
+              }
         </Row>
       </Grid>
       </div>
