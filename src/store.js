7234069a116792a5091b66385d1dda69df5432c5
wojciktomasz <wojciktomasz@o2.pl>
2017-07-11 14:03:48 +0200

    addes to store.js

--- src/store.js
+++ src/store.js
@@ -2,9 +2,11 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 
 import details from './state/details'
+import searchresults from './state/searchresults'
 
 const reducer = combineReducers({
-  details
+  details,
+  searchresults
 })
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
