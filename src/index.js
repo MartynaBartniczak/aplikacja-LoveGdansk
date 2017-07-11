8996b39a14ed6d81931853e24875080a6aa57104
jeziorskilukasz <l.jeziorski@britishclass.pl>
2017-07-11 16:52:28 +0200

    slider correct Grid

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
+import './index.css'
 import store from './store'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/css/bootstrap-theme.css'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
-import './index.css'
 import App from './components/App'
 import EventCalendar from "./components/EventCalendar";
 
