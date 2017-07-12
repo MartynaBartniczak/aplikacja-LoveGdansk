4b59df25fbc0ab3802f0ee0c4e87a965253c46d1
jeziorskilukasz <l.jeziorski@britishclass.pl>
2017-07-12 16:48:17 +0200

    event Details

--- src/components/EventDetails.js
+++ src/components/EventDetails.js
@@ -10,40 +10,71 @@ import MainMenu from "./MainMenu"
 import Time from 'react-time'
 import { PageHeader } from 'react-bootstrap'
 import { connect } from 'react-redux'
-// export default class EventDetails extends React.Component {
-//   render() {
-//     let now = new Date()
-//     return (
-//       <div className="EventDetails">
-//       <DetailsIntro/>
-//         <div>
-//           <p>Dzisiaj jest  <Time value={now} format="YYYY/MM/DD" /></p>
-//         </div>
-//         <PageHeader>Jesteś w Trójmieście <small>zobacz co się dzieje</small></PageHeader>
-//         <DetailsDescription/>
-//         <DetailsMap/>
-//         <EventCalendar/>
-//         <MainMenu />
-//       </div>
-//     );
-//   }
-// }
-
-const EventDetails = ({ value, handleClick, handleDecrementClick }) => (
-  <div>
-    <h1>Smart Counter</h1>
-    <h2>{value}</h2>
-    <button onClick={handleClick}>Increment</button>
-    <button onClick={handleDecrementClick}>Decrement</button>
-  </div>
-)
+import {Grid,
+  Row,
+  Col,
+  Thumbnail,
+  Button} from 'react-bootstrap'
+import './SearchResults.css'
+import { Link } from 'react-router-dom'
+import { fetchSearchResults } from '../state/searchresults'
 
 export default connect(
   state => ({
-    value: state.details.value
+    searchresults: state.searchresults
   }),
   dispatch => ({
-    handleClick: () => dispatch({ type: 'INCREMENT' }),
-    handleDecrementClick: () => dispatch({ type: 'DECREMENT' })
+    fetchSearchResults: () => dispatch(fetchSearchResults())
   })
-)(EventDetails)
+)(
+  class SearchResults extends React.Component {
+
+    componentWillMount() {
+      this.props.fetchSearchResults()
+    }
+
+    render() {
+      const { data, fetching, error } = this.props.searchresults;
+      let now = new Date();
+      return (
+        <div className="mainresults">
+          <Grid>
+            <h1>Jesteś w Trójmieście <small>zobacz co się dzieje</small></h1>
+            <h2><small>szczegóły Twojej imprezy w dniu </small><Time value={now} format="YYYY/MM/DD" /></h2>
+          </Grid>
+          <Grid>
+              { error === null ? null : <p>{error.message}</p> }
+              { fetching === false ? null : <p>Fetching data...</p>}
+              {
+                data !== null && data.slice(1,2).map(
+                  event => (
+
+                <Row className="show-grid">
+                  <Col xs={6} md={6}>
+
+                <h2>Impreza: {event.category}</h2>
+                <h3>Kiedy: {event.startdate} | Godzina: {event.starttime}</h3>
+                <h4>Za ile wjazd: {event.cost} PLN</h4>
+                <p>{event.place} | {event.city}  </p>
+
+                </Col>
+
+                <Col xs={6} md={6}>
+
+                  <h2>Impreza: {event.category}</h2>
+                  <h3>Kiedy: {event.startdate} | Godzina: {event.starttime}</h3>
+                  <h4>Za ile wjazd: {event.cost} PLN</h4>
+                  <p>{event.place} | {event.city}  </p>
+
+                  </Col>
+                </Row>
+                  )
+                )
+              }
+          </Grid>
+
+        </div>
+      )
+    }
+  }
+)
