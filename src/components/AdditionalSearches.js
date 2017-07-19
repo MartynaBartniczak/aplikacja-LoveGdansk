fbe9dfebbc45bd669472cba14198b0924f763d12
wojciktomasz <wojciktomasz@o2.pl>
2017-07-19 16:18:28 +0200

    toggle additional filters working + animation

--- src/components/AdditionalSearches.js
+++ src/components/AdditionalSearches.js
@@ -2,16 +2,17 @@
  * Created by tomaszwojcik on 19.07.17.
  */
 import React from 'react'
-import ToggleDisplay from 'react-toggle-display'
 import EventCategories from "./EventCategories"
 import TimeSlider from "./TimeSlider"
 import LocationSlider from "./LocationSlider"
 import {ButtonToolbar, Button, Grid, Row, Col} from 'react-bootstrap'
+import {CSSTransitionGroup} from 'react-transition-group'
+import './AddtionalSearches.css'
 
 export default class AdditionalSearches extends React.Component {
   constructor() {
     super();
-    this.state = { show: false };
+    this.state = {show: false};
   }
 
   handleClick() {
@@ -19,6 +20,7 @@ export default class AdditionalSearches extends React.Component {
       show: !this.state.show
     });
   }
+
   render() {
     return (
       <div>
@@ -28,15 +30,26 @@ export default class AdditionalSearches extends React.Component {
             bsStyle="btn-block btn-warning btn-md btn-new">
             Filtry dodatkowe
           </Button>
-          <ToggleDisplay show={this.state.show}>
-          <Row>
-            <TimeSlider/>
-            <LocationSlider/>
-          </Row>
-          <Row>
-            <EventCategories/>
-          </Row>
-          </ToggleDisplay>
+
+          <CSSTransitionGroup
+            transitionName="example"
+            transitionEnterTimeout={500}
+            transitionLeaveTimeout={500}>
+            {
+              this.state.show === false ?
+                null :
+                <div>
+                  <Row>
+                    <TimeSlider/>
+                    <LocationSlider/>
+                  </Row>
+                  <Row>
+                    <EventCategories/>
+                  </Row>
+                </div>
+            }
+          </CSSTransitionGroup>
+
         </Grid>
       </div>
     )
