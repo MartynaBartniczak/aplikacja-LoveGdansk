99df4a5bfb5e72697ea55f2bf503824ae8403c6a
jeziorskilukasz <l.jeziorski@britishclass.pl>
2017-07-21 09:44:44 +0200

    firebase

--- src/index.js
+++ src/index.js
@@ -9,6 +9,17 @@ import "react-big-calendar/lib/css/react-big-calendar.css";
 import App from "./components/App";
 import "react-fontawesome";
 import { LoginLink } from 'react-stormpath';
+import firebase from 'firebase'
+
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCCIYZTEiOvWsmHy9F3s1RNuKh0YYDEW1g",
+  authDomain: "jfdd7-app-jeziorski.firebaseapp.com",
+  databaseURL: "https://jfdd7-app-jeziorski.firebaseio.com",
+  projectId: "jfdd7-app-jeziorski",
+  storageBucket: "jfdd7-app-jeziorski.appspot.com",
+  messagingSenderId: "385655041946"
+})
 
 ReactDOM.render(
   <Provider store={store}>
