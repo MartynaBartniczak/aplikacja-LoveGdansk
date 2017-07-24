31085ff75c9449bd90dcc36439d78d67f136fc0b
MartynaBartniczak <aszkee@gmail.com>
2017-07-24 16:29:42 +0200

    uzupelniony auth

--- src/state/auth.js
+++ src/state/auth.js
@@ -1,9 +1,23 @@
+import firebase from 'firebase'
+import {initFavsSync} from './favevent'
+
+
 const SYNC_USER = 'auth/SYNC_USER'
+
 
 export const syncUser = (user) => ({
   type: SYNC_USER,
   user
 })
+
+export const initUserSync = () => dispatch => firebase.auth().onAuthStateChanged(
+  user => {
+    if (user) {
+      dispatch(initFavsSync())
+    }
+    dispatch(syncUser(user))
+  }
+)
 
 const initialState = {
   user: null
